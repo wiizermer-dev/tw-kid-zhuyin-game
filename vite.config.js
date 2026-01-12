42a858a133dc27e0ae26c67d34cbc480b6e07126
Jimmy Tang <jimmy.tang@turingspace.co>
2026-01-12 17:05:19 +0800

    fix: remove base path for vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/tw-kid-zhuyin-game/',
+  // base: '/tw-kid-zhuyin-game/', // Vercel 不需要這個，GitHub Pages 才需要
 })
